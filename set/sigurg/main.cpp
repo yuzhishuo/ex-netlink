072f27339fe594322cd8784339101837558b19ff
Watermelon <aoumeior@outlook.com>
2022-08-30 02:56:37 +0000

    recv msg : 123
        localhost.4567 > localhost.1234: Flags [S], cksum 0xfe30 (incorrect -> 0x253a), seq 688835183, win 65495, options [mss 65495,sackOK,TS val 1324929448 ecr 0,nop,wscale 7], length 0
    02:55:43.795741 IP (tos 0x0, ttl 64, id 0, offset 0, flags [DF], proto TCP (6), length 60)
        localhost.1234 > localhost.4567: Flags [S.], cksum 0xfe30 (incorrect -> 0x2824), seq 2567062381, ack 688835184, win 65483, options [mss 65495,sackOK,TS val 1324929448 ecr 1324929448,nop,wscale 7], length 0
    02:55:43.795754 IP (tos 0x0, ttl 64, id 53593, offset 0, flags [DF], proto TCP (6), length 52)
        localhost.4567 > localhost.1234: Flags [.], cksum 0xfe28 (incorrect -> 0x4ee0), ack 1, win 512, options [nop,nop,TS val 1324929448 ecr 1324929448], length 0
    02:55:43.795785 IP (tos 0x0, ttl 64, id 53594, offset 0, flags [DF], proto TCP (6), length 56)
        localhost.4567 > localhost.1234: Flags [P.U], cksum 0xfe2c (incorrect -> 0xea49), seq 1:5, ack 1, win 512, urg 4, options [nop,nop,TS val 1324929448 ecr 1324929448], length 4
    02:55:43.795799 IP (tos 0x0, ttl 64, id 50017, offset 0, flags [DF], proto TCP (6), length 52)
        localhost.1234 > localhost.4567: Flags [.], cksum 0xfe28 (incorrect -> 0x4edc), ack 5, win 512, options [nop,nop,TS val 1324929448 ecr 1324929448], length 0
    02:55:43.796943 IP (tos 0x0, ttl 64, id 50018, offset 0, flags [DF], proto TCP (6), length 52)
        localhost.1234 > localhost.4567: Flags [R.], cksum 0xfe28 (incorrect -> 0x4ed7), seq 1, ack 5, win 512, options [nop,nop,TS val 1324929449 ecr 1324929448], length 0

---
+++ set/sigurg/main.cpp
@@ -0,0 +1,106 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+
+using namespace std;
+
+condition_variable cv;
+mutex mt;
+atomic_bool ck = {false};
+
+void server() {
+  auto fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (fd == -1) {
+    printf("create socket err: %s \n", strerror(errno));
+  }
+
+  struct sockaddr_in addr;
+  bzero(&addr, sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(1234);
+  addr.sin_addr.s_addr = INADDR_ANY;
+
+  if (auto e = bind(fd, (struct sockaddr *)&addr, sizeof(addr)); e != 0) {
+    printf("create bind err: %s \n", strerror(errno));
+  }
+
+  if (auto e = listen(fd, 100); e != 0) {
+    printf("create listen err: %s \n", strerror(errno));
+  }
+
+  ck = true;
+  cv.notify_one();
+  while (true) {
+    auto connectfd = accept(fd, nullptr, nullptr);
+    if (connectfd == -1) {
+      printf("create accept connectfd err: %s \n", strerror(errno));
+    }
+
+    char buf[1024] = {0};
+    if (auto e = recv(connectfd, buf, sizeof(buf), 0); e == -1) {
+      printf("recv err: %s \n", strerror(errno));
+    }
+
+    printf("recv msg : %s\n", buf);
+    close(connectfd);
+  }
+}
+
+void client() {
+  auto fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (fd == -1) {
+    printf("create socket err: %s \n", strerror(errno));
+  }
+
+  struct sockaddr_in addr;
+  bzero(&addr, sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(4567);
+  addr.sin_addr.s_addr = INADDR_ANY;
+
+  if (auto e = bind(fd, (struct sockaddr *)&addr, sizeof(addr)); e != 0) {
+    printf("create bind err: %s\n", strerror(errno));
+  }
+
+  struct sockaddr_in addr1;
+  bzero(&addr, sizeof(addr1));
+  addr1.sin_family = AF_INET;
+  addr1.sin_port = htons(1234);
+  addr1.sin_addr.s_addr = inet_addr("127.0.0.1");
+  {
+    std::unique_lock<std::mutex> ul(mt);
+    cv.wait(ul, [&]() { return ck == true; });
+  }
+  if (auto e = connect(fd, (struct sockaddr *)&addr1, sizeof(addr1)); e != -1) {
+    char buf[] = {'1', '2', '3', '4'};
+    if (auto se = send(fd, buf, sizeof(buf), MSG_OOB); se == -1) {
+      printf("create send err: %s \n", strerror(errno));
+    }
+    // close(fd);
+  } else {
+    printf("create connect err: %s \n", strerror(errno));
+  }
+}
+
+int main(int argc, char const *argv[]) {
+  thread t1{server};
+  thread t2{client};
+
+  t1.detach();
+  t2.detach();
+
+  while (1) {
+  }
+
+  return 0;
+}
